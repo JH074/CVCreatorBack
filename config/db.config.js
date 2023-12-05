8ee3f7815f2039e933d42a24855cde81c7db38d8
JH074 <00162622@uca.edu.sv>
2023-12-05 14:06:37 -0600

    feat:V1.0.2

--- config/db.config.js
+++ config/db.config.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 
 const envconfig = require('./env.config')
 
-const uri = envconfig.MONGO_URI;
+const dburi = process.env.MONGO_URI
+
 
 const connect = async () => {
     try {
-      await mongoose.connect(uri);
+      await mongoose.connect(dburi);
       debug('Connected successfully to database!');
     } catch (error) {
       debug("[Error]: Can't connect to database!");
